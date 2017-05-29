921a06a40d2c63ed277504b2592799694a95ebcc
Colin Bloom <colinjbloom113@gmail.com>
2017-05-29 16:12:59 -0700

    add click listener to button with toast

--- app/src/main/java/com/epicodus/peaceofmind/MainActivity.java
+++ app/src/main/java/com/epicodus/peaceofmind/MainActivity.java
@@ -2,12 +2,29 @@ package com.epicodus.peaceofmind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mLocationInputHome;
+    private Button mLocationInputHomeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocationInputHome = (EditText) findViewById(R.id.locationInputHome);
+        mLocationInputHomeButton = (Button) findViewById(R.id.locationInputHomeButton);
+
+        mLocationInputHomeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
